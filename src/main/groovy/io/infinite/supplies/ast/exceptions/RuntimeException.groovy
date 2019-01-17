868f8213fcc1b87cbd5970730ffb7c16773afd78
apryamostanov <anton.pryamostanov@gmail.com>
2019-01-17 18:22:32 +0400

    Add missing Runtime exception constructor

--- src/main/groovy/io/infinite/supplies/ast/exceptions/RuntimeException.groovy
+++ src/main/groovy/io/infinite/supplies/ast/exceptions/RuntimeException.groovy
@@ -1,5 +1,8 @@
 package io.infinite.supplies.ast.exceptions
 
+import io.infinite.supplies.ast.other.ASTUtils
+import org.codehaus.groovy.ast.ASTNode
+
 class RuntimeException extends Exception {
 
     RuntimeException(String var1) {
@@ -10,4 +13,10 @@ class RuntimeException extends Exception {
         super(var1, exception)
         this.setStackTrace([] as StackTraceElement[])
     }
+
+    RuntimeException(ASTNode astNode, Exception exception) {
+        super(new ASTUtils().prepareExceptionMessage(astNode), exception)
+        this.setStackTrace([] as StackTraceElement[])
+    }
+
 }
